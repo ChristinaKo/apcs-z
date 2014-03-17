47767a76637c9105d461805148711296bb83ed3b
ChristinaKo <christinako822@gmail.com>
2014-03-17 00:22:46 -0400

    tried the hw, doesnt work

--- 07-quick-sort/Sort.java
+++ 07-quick-sort/Sort.java
@@ -47,10 +47,57 @@ public class Sort{
 	    }
 		return a;
 	}
+	
     }
+    public int partition(int[] a, int L, int R){
+	/*int wall = L+1;
+	int p = a[(L+R)/2];
+	a[(L+R)/2] = a[R];
+	a[R] = p;
+	*/
+	int p = a[(L+R)/2];
+	int wall = L;
+	int tmp = a[R];
+	a[(L+R)/2] = tmp;
+	a[R] = p;
+	for (int i=L; i< R;L++){
+	    if(a[i] < p){
+		tmp = a[i];
+		a[i] = a[wall];
+		a[wall] = tmp;
+		wall = wall + 1;
+	    }
+	}
+	tmp = a[wall];
+	a[wall] = a[R];
+	a[R] = tmp;
+	return wall;
+    }
+
+    public void qsort2(int[] a, int l, int r){
+	if (r-1 <= 1){
+	    return;
+	}
+	else{
+	    int p =partition(a, l ,r);
+	    if (p -1>1){
+		qsort2(a,l,p-1);
+	    }
+	    if (p +1<r){
+		qsort2(a,p+1,r);
+	    }
+	}
+		
+    }      
+	
     public static void main(String[] args){
 	Sort s = new Sort();
 	int[] x = {14,52,6,234,15,2,3,124,52,3,1,35,94,123};
+	int[] x2 = {13,25,1,5,74,2,73,79,253,745,123,243,12};
 	System.out.println(Arrays.toString(s.qsort(x)));
+	s.qsort2(x2,0,x2.length-1);
+	System.out.println(Arrays.toString(x2));
+
     }
+
 }
