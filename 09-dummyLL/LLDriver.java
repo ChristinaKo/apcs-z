b4f0a587285538457698d82a0aa8642d62b31a5c
ChristinaKo <christinako822@gmail.com>
2014-03-24 00:13:49 -0400

    confused a bit

---
+++ 09-dummyLL/LLDriver.java
@@ -0,0 +1,25 @@
+public class LLDriver{
+    public static void main(String[] args){
+	MyLinkedList L = new MyLinkedList();
+	L.add("Sully");
+	//System.out.println(L);
+	L.add("Mike");
+	L.add("Randall");
+	L.add("johnson");
+	System.out.println(L);
+	System.out.println(L.get(1));
+	System.out.println(L.length());
+	System.out.println(L.set(2, "Max"));
+	System.out.println(L);
+	System.out.println(L.remove(2));
+	System.out.println(L);
+	L.add("one");
+	L.add("three");
+	L.add("two");
+	L.add(2,"Christina");
+	System.out.println(L);
+	System.out.println(L.find("three"));
+ 
+
+    }
+}
---
+++ 09-dummyLL/MyLinkedList.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+public class MyLinkedList{
+    private Node head;
+    public MyLinkedList(){
+	head = new Node("dum");
+    }
+    public void add(String d){
+	Node tmp = new Node(d);
+	//head=tmp;
+	tmp.setNext(head.getNext());
+	head.setNext(tmp);
+    }
+    public void add(int i, String s){
+	int count = 0;
+	Node tmp = new Node(s);
+	for (Node n = head; n != null; n = n.getNext()){
+	    if (count ==i){
+		tmp.setNext(n.getNext());
+		n.setNext(tmp);
+		
+	    }
+	    count =count +1;
+	}
+    }
+    public String get(int i){
+	int count = 0;
+	for (Node n = head; n != null; n = n.getNext()){
+	    if (count == i){
+		return n.getData();
+
+	    }
+	    count = count + 1;
+	}
+	throw new IndexOutOfBoundsException();
+    }
+    public String set(int i, String s){
+	int count = 0;
+	Node tmp = new Node();
+	Node nn = new Node(s);
+	for (Node n = head; n!= null; n=n.getNext()){
+	    if (count == i-1){ 
+		tmp = new Node(n.getNext().getData());
+		n.setNext(nn);
+	    }
+	    count = count +1;
+	}
+	return tmp.getData();
+    }
+    public String remove(int i){
+	Node tmp1= new Node();
+	Node tmp2 = new Node();
+	int count = 0;
+	for(Node j = head ;j!= null; j = j.getNext()){
+	    if (count == i-1){
+		tmp1 = j;
+		tmp2 = j.getNext();
+		tmp1.setNext(tmp1.getNext().getNext());
+	    }
+	    count = count +1;
+	}
+	return tmp2.getData();
+    }
+
+    public int find(String s){
+	int ind = 0;
+	for(Node n= head; n!= null; n=n.getNext()){
+	    if(n.getData().equals(s)){
+		return ind;
+	    }
+	    ind = ind + 1;
+	}
+	return -1;
+    }
+    public int length(){
+	int len = 0;
+	for (Node n = head; n!= null; n=n.getNext()){
+	    len = len + 1;
+	}
+	return len;
+    }
+    public String toString(){
+	String s = "";
+	for (Node n = head; n!= null; n = n.getNext()){
+	    s = s  + n + "  ";
+	}
+	return s;
+    }
+}
