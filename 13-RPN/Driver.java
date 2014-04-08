dca5d831c65e766cc5e141d3af5c27268d38d9b6
ChristinaKo <christinako822@gmail.com>
2014-04-08 09:02:44 -0400

    fixed

--- 13-RPN/Driver.java
+++ 13-RPN/Driver.java
@@ -2,10 +2,13 @@ public class Driver{
     public static void main(String[] args){
 	MyStack m = new MyStack();
 	m.calc("123");
+	System.out.println(m);
 	m.push("132");
+	System.out.println(m);
 	m.calc("+");
 	System.out.println(m);
 	m.calc("1245561");
+	System.out.println(m);
 	m.calc("/");
 	System.out.println(m);
 	m.calc("24");
